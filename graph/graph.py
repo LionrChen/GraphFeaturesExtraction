42030e689703757771fca14f77675ea2ac30462b
chenliang <cl@chenliang.com>
2019-07-21 13:10:52 +0800

    Code upgrade with first test.

--- graph/graph.py
+++ graph/graph.py
@@ -21,15 +21,23 @@ __mtime__ = '2019/7/16'
 """
 from node import Node
 
+
 class Graph:
     def __init__(self):
-        self.nodes = {}
+        self.nodes = dict()
 
     def add_node(self, id):
         if id not in self.nodes.keys():
             new_node = Node(id)
             self.nodes[id] = new_node
             return new_node
+        else:
+            print("Node already exits.")
+
+    def add_node_object(self, node):
+        if node not in self.nodes:
+            self.nodes[node.node_id] = node
+            return node
         else:
             print("Node already exits.")
 
@@ -47,12 +55,98 @@ class Graph:
         for k, v in iter(self.nodes):
             v.reset()
 
-    def add_edge(self, sor, terg):
-        if terg not in self.nodes[sor].neighbors:
-            self.nodes[sor].neighbors.append(terg)
-            print("Add edge to node {}, e: {} -> {}".format(sor,sor,terg))
+    def add_edge(self, sour, terg):
+        if terg not in self.nodes[sour].neighbors:
+            self.nodes[sour].neighbors.append(terg)
+            print("Add edge to node {}, e: {} -> {}".format(sour, sour, terg))
         else:
             print("The edge already exits.")
 
     def __iter__(self):
         print("Number of node: {}.".format(self.nodes.__len__()))
+
+
+def SCC(sour, targ, graph):
+    """ calculate the number of strongly connected components within neighborhood-subgraph.
+        First generate 1-dimensional neighborhood-subgraph with source and target node.
+    """
+    neighborhood_graph = graph()
+    neighborhood_graph.add_node(sour)
+    neighborhood_graph.add_node(targ)
+    # add nodes
+    for node in graph[sour].neighbors:
+        if node not in neighborhood_graph.nodes:
+            neighborhood_graph.add_node_object(node)
+    for node in graph[targ].neighbors:
+        if node not in neighborhood_graph.nodes:
+            neighborhood_graph.add_node_object(node)
+    # add edges, check out each node from source graph to estimate whether exits edge among them.
+    for node in neighborhood_graph.nodes.keys():
+        for nei in neighborhood_graph[node].neighbors:
+            if nei not in neighborhood_graph.nodes.keys():
+                del nei
+
+    # reverse graph
+    def reverse_graph(subgraph):
+        re_graph = dict()
+        for key in subgraph.keys():
+            re_graph[key] = subgraph.get(key, set())
+        # reverse edge
+        for key in subgraph.keys():
+            for nei in subgraph[key]:
+                re_graph[nei].neighbors.add(key)
+        return re_graph
+
+    # gain a sequence sorted by time
+    def topo_sort(subgraph):
+        res = []
+        S = set()
+
+        # Depth-first traversal/search
+        def dfs(subgraph, u):
+            if u in S:
+                return
+            S.add(u)
+            for v in subgraph[u].neighbors:
+                if v in S:
+                    continue
+                dfs(subgraph, v)
+            res.append(u)
+
+        # check each node was leave out
+        for u in subgraph.keys():
+            dfs(subgraph, u)
+
+        res.reverse()
+        return res
+
+    # gain singe strongly connected components with assigns start node
+    def walk(subgraph, s, S=None):
+        if S is None:
+            s = set()
+        Q = []
+        P = dict()
+        Q.append(s)
+        P[s] = None
+        while Q:
+            u = Q.pop()
+            for v in subgraph[u].neighbors:
+                if v in P.keys() or v in S:
+                    continue
+                Q.append(v)
+                P[v] = P.get(v, u)
+        return P
+
+    # use to record the node of strongly connected components
+    seen = set()
+    # to store scc
+    scc = []
+    GT = reverse_graph(neighborhood_graph)
+    for u in topo_sort(neighborhood_graph):
+        if u in seen:
+            continue
+        C = walk(GT, u, seen)
+        seen.update(C)
+        scc.append(sorted(list(C.keys())))
+
+    print(scc)
--- graph/readFile.py
+++ graph/readFile.py
@@ -19,16 +19,30 @@ __mtime__ = '2019/7/16'
                ┃┫┫  ┃┫┫
                ┗┻┛  ┗┻┛
 """
+import csv
 
 from graph import Graph
+# origin file
+data_file = open("../dataset/digg_friends.csv", 'r')
+csv_read_file = csv.reader(data_file)
 
-data_file = open("../dataset/soc-sign-bitcoinotc.csv", 'r')
-fc = data_file.readlines()
+# The file use to write extracted features.
+output_file = open("../dataset/digg_friends_features.csv", 'w')
+csv_write = csv.writer(output_file, dialect='excel')
+
 graph = Graph()
-for line in fc:
-    data_f = line.split(",")
-    # print("Source: {}, Target: {}".format(data_f[0],data_f[1]))
-    graph.add_node(data_f[0])
-    graph.add_node(data_f[1])
-    graph.add_edge(data_f[0],data_f[1])
+
+for line in csv_read_file:
+    graph.add_node(line[2])
+    graph.add_node(line[3])
+    if line[0] is 1 or '1':
+        graph.add_edge(line[2], line[3])
+
 graph.__iter__()
+
+for line1 in csv_read_file:
+    # feature: is-friend,in-degree,
+    csv_write.writerow([int(line1[0]), graph.nodes.get(line1[2]).neighbors.__len__()])
+    print([int(line1[0]), graph.nodes.get(line1[2]).neighbors.__len__()])
+
+print("Write over.")
