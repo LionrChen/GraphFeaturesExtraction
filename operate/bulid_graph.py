b3d87f2b77f3921dc6f06ff6fb4b9cf82d59fa67
LionrChen <chensir88@outlook.com>
2019-11-05 11:18:19 +0800

    Modifiy files friend measure.

--- operate/bulid_graph.py
+++ operate/bulid_graph.py
@@ -5,24 +5,10 @@
 # @File    : bulid_graph.py
 # @Software: PyCharm
 
-import igraph as ig
+import networkx as nx
 import csv
 import numpy as np
 import pandas as pd
-from SCC import SCC
-
-
-# graph = ig.Graph.Read_Edgelist("../dataset/digg_friends.csv", directed=False)
-# neighbors = graph.neighborhood()
-# print(type(neighbors))
-
-
-# origin file
-# data_file = open("../dataset/testdata.csv", 'r')
-# csv_read_file = csv.reader(data_file)
-
-# read file use numpy
-# data_file_np = np.loadtxt("../dataset/testdata.csv", delimiter=",", usecols=(0, 2, 3))
 
 
 # read file use pandas
@@ -40,19 +26,62 @@ def convert_currency(value):
 data_file_pd = pd.read_csv("../dataset/digg_friends_format.csv", usecols=(0, 2, 3), header=None)
 # data_file_pd.apply(convert_currency)
 # The file use to write extracted features.
-output_file = open("../dataset/digg_friends_features.csv", 'w')
+output_file = open("blog_data_FM.csv", 'w')
 csv_write = csv.writer(output_file, dialect='excel')
 
-vertices = set()
+vertices = list(set((data_file_pd.iloc[2]).tolist() + (data_file_pd.iloc[3]).tolist()))
 edges = []
 
 for index, row in data_file_pd.iterrows():
     edges.append((row[2], row[3]))
 
-
 # graph.add_vertices(list(vertices))
 # graph.add_edges(edges)
-graph = ig.Graph.TupleList(edges,directed=False, vertex_name_attr='name', edge_attrs=None, weights=False)
+graph = nx.Graph()
+graph.add_nodes_from(vertices)
+print("Vertices: {}, and number is {}".format(vertices[:3], vertices.__len__()))
+graph.add_edges_from(edges)
+print("edges: {}, and number is {}".format(edges[:3], edges.__len__()))
+print("number of edges: {}".format(graph.number_of_edges()))
+total_edges = graph.number_of_edges()
+processed_edges = 0
+
+for index, row in data_file_pd.iterrows():
+    # feature: is-friend, friend-measure,
+    # subgraph_list = set(list(graph.neighbors(row[2])) + list(graph.neighbors(row[3])))
+    # subgraph = graph.subgraph(list(subgraph_list))
+    nbrs_node1 = list(graph.neighbors(row[2]))
+    nbrs_node2 = list(graph.neighbors(row[3]))
+    F_m = 0
+    for node1 in nbrs_node1:
+        if node1 is not row[3]:
+            nbrs = set(graph.neighbors(node1))
+            nbrs.add(node1)
+            F_m += len(set(nbrs_node2).difference(nbrs))
+    csv_write.writerow([F_m])
+    # for u, v, PA in nx.preferential_attachment(graph, [(row[2], row[3])]):
+    #     csv_write.writerow([PA])
+    processed_edges += 1
+    print("{}: Node {} and {} has processed. {} edges the remaining.".format(processed_edges, row[2], row[3],
+                                                                             total_edges - processed_edges))
+
+# for node, nbrs in graph.adjacency():
+#     for nbr in nbrs:
+#         # F_m = 0
+#         # for nb_nbr in graph.neighbors(nbr):
+#         #     if nb_nbr not in nbrs and not graph.edges([nbr, nb_nbr]):
+#         #         F_m += 1
+#         # feature: is-friend, in-degree, SCC, total-friends, Jaccard-coefficient, preference-attachment, friend-measure
+#         # ,
+#         #                                len(set(graph.neighbors(node) + graph.neighbors(nbr))), nx.jaccard_coefficient(graph, [node, nbr]),
+#         #                                nx.preferential_attachment(graph, [node, nbr]), F_m
+#         subgraph = list(graph.neighbors(node)) + list(graph.neighbors(nbr))
+#         try:
+#             csv_write.writerow([1, nx.degree(graph, node), nx.number_strongly_connected_components(graph.subgraph(subgraph).to_directed())])
+#             processed_edges += 1
+#             print("{}: Node {} and {} has processed. {} edges the remaining.".format(processed_edges, node, nbr, total_edges-processed_edges))
+#         except:
+#             print("Node {} has generate exception.".format(node))
 # SCCs = {}
 # for ver in graph.vs:
 #     subgraph = graph.subgraph(graph.neighbors(ver))
@@ -66,11 +95,14 @@ graph = ig.Graph.TupleList(edges,directed=False, vertex_name_attr='name', edge_a
 #         temp_graph[key] = nei
 #     print(temp_graph)
 #     SCCs[ver["name"]] = SCC(temp_graph).__len__()
-numbers = graph.indegree()
-for index, row in data_file_pd.iterrows():
-    # feature: is-friend,in-degree,
-    csv_write.writerow([row[0], numbers[row[3]]])
-graph.neighbors(5)
-vertic = graph.get
-graph.subgraph(5)
+# numbers = graph.indegree()
+# print(numbers)
+# for index, row in data_file_pd.iterrows():
+#     # feature: is-friend,in-degree,
+#     try:
+#         csv_write.writerow([row[0], numbers[index]])
+#     except:
+#         print("list index {} out of range.".format(index))
+# print(graph.neighbors(5))
+# print(graph.subgraph(5))
 print("Write over.")
